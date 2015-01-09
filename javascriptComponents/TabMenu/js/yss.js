5546ad0e013210ac4c30eec18bcd6a85a9356975
zsl99a <1033392732@qq.com>
2015-01-09 14:00:34 +0800

    tab

---
+++ javascriptComponents/TabMenu/js/yss.js
@@ -0,0 +1,71 @@
+(function ($) {
+
+"use struct";
+
+var TabMenu = function (box, opt) {
+  if (box.tabMenu) return;
+
+  var i, me = this,
+      defOpt = {
+        ts: '>.titBox>li', /* 标题 */
+        bs: '>.byBox>li', /* 内容 */
+        fn: null, /* 回调，异步加载需用给定此函数 */
+        arg: {} /* 回调函数的参数 */
+      };
+  opt = $.extend({}, defOpt, opt);
+  me._init(box, opt);
+
+  /* 绑定事件 */
+  this._tClick();
+
+  /* 自定义设置接口 */
+  return function (optc) {
+    opt = $.extend({}, opt, optc);
+    me._init(box, opt);
+  };
+}
+
+TabMenu.prototype._init = function (box, opt) {
+  var $box = $(box);
+  
+  this.$ts = $box.find(opt.ts);
+  this.$bs = $box.find(opt.bs);
+  this.fn  = opt.fn;
+  this.arg = opt.arg;
+
+  /* 给标题指定下标，与内容区对应 */
+  this.$ts.each(function (i, box) {
+    $(box).data('tabPage', i);
+  });
+}
+
+TabMenu.prototype._tClick = function () {
+  var me = this;
+  this.$ts.on('click', function () {
+    var $this = $(this);
+    $this.addClass('action').siblings().removeClass('action');
+    me.$bs.eq($this.data('tabPage')).addClass('action').siblings().removeClass('action');
+    if ('function' === typeof me.fn) {
+      me.fn.apply($this, me.arg);
+    }
+  });
+  me.$ts.eq(0).trigger('click');
+}
+
+/*
+  以下为jQuery包装函数
+*/
+
+$.fn.tabMenu = function (opt) {
+  var i;
+  this.each(function (opt) {
+    if (!this.tabMenu) {
+      this.tabMenu = new TabMenu(this, opt);
+    } else {
+      this.tabMenu(opt);
+    }
+  }, [opt]);
+  return this;
+}
+
+})(jQuery);
